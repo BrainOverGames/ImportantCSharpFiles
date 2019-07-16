2ef6174e04eeb5432b3c1926a6f7e1b6ce0c10b7
Rishabh Sharma <30407235+BrainOverGames@users.noreply.github.com>
2019-07-16 22:10:03 +0530

    Add files via upload
    EventsActionsBestPractices Project

---
+++ Files/Assets/Scripts/CurrencyHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EventsActionsBestPractices
+{
+    public static class CurrencyHandler
+    {
+        public static Action<int> OnKeysChangeEvent;
+
+        public static int GetKeys
+        {
+            get
+            {
+                return GameProgressHandler.gameProgressData.gameData.keys;
+            }
+        }
+
+        public static bool DeductKeys(int amount)
+        {
+            if(GameProgressHandler.gameProgressData.gameData.keys >= amount)
+            {
+                GameProgressHandler.gameProgressData.gameData.keys -= amount;
+                GameProgressHandler.SaveData();
+                OnKeysChangeEvent?.Invoke(GetKeys);
+                return true;
+            }
+            return false;
+        }
+
+        public static void CreditKeys(int amount)
+        {
+            GameProgressHandler.gameProgressData.gameData.keys += amount;
+            GameProgressHandler.SaveData();
+            OnKeysChangeEvent?.Invoke(GetKeys);
+        }
+
+        public static void RegisterOnKeysChangeEvent(Action<int>Callback, bool triggerCallback = false)
+        {
+            OnKeysChangeEvent += Callback;
+            if(triggerCallback)
+            {
+                Callback(GetKeys);
+            }
+        }
+
+        public static void UnRegisterOnKeysChangeEvent(Action<int>Callback)
+        {
+            if(Callback != null)
+            {
+                OnKeysChangeEvent -= Callback;
+            }
+        }
+    }
+}
+
---
+++ Files/Assets/Scripts/GameProgressHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace EventsActionsBestPractices
+{
+    public class GameProgressHandler : MonoBehaviour
+    {
+        const string gameProgressString = "gameprogress";
+
+        public static Data gameProgressData;
+
+        private void Awake()
+        {
+            //PlayerPrefs.DeleteAll();
+            ReadLocalData();
+        }
+
+        void ReadLocalData()
+        {
+            if (!PlayerPrefs.HasKey(gameProgressString))
+            {
+                SaveDefaultProgress();
+            }
+            else
+            {
+                LoadData();
+            }
+        }
+
+        private void SaveDefaultProgress()
+        {
+            gameProgressData = new Data();
+            SaveData();
+        }
+
+        public static void LoadData()
+        {
+            Debug.LogError(gameProgressData);
+            //data decryption can be added 
+            gameProgressData = JsonUtility.FromJson<Data>(PlayerPrefs.GetString(gameProgressString));
+            //PlayerPrefs.GetString(gameProgressString);
+        }
+
+        public static void SaveData()
+        {
+            Debug.LogError(JsonUtility.ToJson(gameProgressData));
+            //data encryption can be added
+            PlayerPrefs.SetString(gameProgressString, JsonUtility.ToJson(gameProgressData));
+            //PlayerPrefs.SetString(gameProgressString, "GameProgress");
+        }
+    }
+}
+
