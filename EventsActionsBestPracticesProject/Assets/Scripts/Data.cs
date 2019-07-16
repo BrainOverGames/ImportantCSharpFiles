68d52d912ef35f8eab5c957333e35ac300149a73
Brain Over Games <rishabhsharma24081992@gmail.com>
2019-07-16 22:20:11 +0530

    EventsActionsBestPracticesProject committed

---
+++ EventsActionsBestPracticesProject/Assets/Scripts/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace EventsActionsBestPractices
+{
+    [Serializable]
+    public class GameData
+    {
+        public int keys;
+
+        public GameData()
+        {
+            keys = 1;
+        }
+    }
+
+    [Serializable]
+    public class Data
+    {
+        public GameData gameData;
+        public Data()
+        {
+            gameData = new GameData();
+        }
+    }
+}
+
